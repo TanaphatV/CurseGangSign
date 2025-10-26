ab87b64c38eff43c8c97c24885dfcb948285530d
Sunny <sunny@bitegginc.com>
2025-10-26 22:50:48 +0700

    Enemy Nav Mesh Test

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    public NavMeshAgent agent;
+    public Transform player;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 playerPosition = player.position;
+
+        agent.SetDestination(playerPosition);
     }
 }
