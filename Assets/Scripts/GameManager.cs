e8eb919e9b8aadbf90770ea365245b72611ec72b
PaobabaProg <witchakonwatsana@gmail.com>
2025-10-27 23:26:26 +0700

    Health System Scripts (Not finished yet)
    I follow a tutorial on this, But cant put it to use yet tho sorry

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gameManager { get; private set; }
+    public UnitHealth _playerHealth = new UnitHealth(100, 100);
+    void Awake()
+    {
+        if (gameManager != null && gameManager != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            gameManager = this;
+        }
+
+    }
+}
