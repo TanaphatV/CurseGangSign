352b959f42836e761ed5470f92e4cf071d2d6220
Tanaphat Vivattanakijcharoen <tanaphatviva@gmail.com>
2025-10-29 00:19:17 +0700

    damage system

--- Assets/Scripts/SpellFunctionHub.cs
+++ Assets/Scripts/SpellFunctionHub.cs
@@ -2,19 +2,31 @@ using UnityEngine;
 
 public class SpellFunctionHub : MonoBehaviour
 {
+    public int damagePerShot;
     public void Fire()
     {
-
+        Fire(DamageType.fire); 
     }
 
     public void Water()
     {
-
+        Fire(DamageType.water);
     }
 
     public void Electric()
     {
+        Fire(DamageType.lightning);
+    }
 
+    private void Fire(DamageType damageType)
+    {
+        if(Physics.Raycast(Camera.main.ScreenPointToRay(new Vector2(Screen.width/2.0f,Screen.height/2.0f)),out RaycastHit hitInfo))
+        {
+            if(hitInfo.collider.TryGetComponent(out Enemy enemy))
+            {
+                enemy.TakeDamage(damageType,damagePerShot);
+            }
+        }
     }
 
     public void Exorcise()
